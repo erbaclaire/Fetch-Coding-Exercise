f3ff0859b36d865f09d9bcef32279a141902c27e
Claire Erba <56267696+erbaclaire@users.noreply.github.com>
2021-05-24 23:27:14 -0500

    Add files via upload

---
+++ Code/sql_query.sql
@@ -0,0 +1,11 @@
+-- When considering average spend from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater?
+-- When considering total number of items purchased from receipts with 'rewardsReceiptStatus’ of ‘Accepted’ or ‘Rejected’, which is greater
+
+SELECT AVERAGE(Receipts.totalSpent) AS avgSpend, SUM(ReceiptItems.totalItemsPurchased) 
+FROM Receipts
+LEFT JOIN (SELECT COUNT(*) AS totalItemsPurchased 
+	       FROM ReceiptItems
+           GROUP BY _id) AS ReceiptItems
+ON Receipt._id = ReceiptItems._id
+WHERE rewardsReceiptStatus IN ('Accepted', 'Rejected')
+GROUP BY rewardsReceiptStatus;
